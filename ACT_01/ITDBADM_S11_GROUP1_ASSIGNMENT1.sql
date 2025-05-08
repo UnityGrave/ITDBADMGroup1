3612304d551ee68bda3ccdcbd9bbe885467a39b1
olvjar <43496985+olvjar@users.noreply.github.com>
2025-05-08 13:10:14 +0800

    feat(ACT1): add task 1

---
+++ ACT_01/ITDBADM_S11_GROUP1_ASSIGNMENT1.sql
@@ -0,0 +1,36 @@
+/* GROUP 1
+	GENOTA, Kean
+	GRINO, Gem
+    SANTOS, Jerome
+*/
+
+-- (1) Big Brother requests that you design and make a database of his housemates.
+
+-- INIT PBB_Collab DB
+CREATE DATABASE IF NOT EXISTS pbb_collab;
+USE pbb_collab;
+
+-- TABLES
+CREATE TABLE housemates(
+	id INT NOT NULL,
+	firstName VARCHAR(100) NOT NULL,
+    lastName  VARCHAR(100) NOT NULL,
+	gender VARCHAR(20) NOT NULL,
+    age INT NOT NULL,
+    hometown VARCHAR(200) NOT NULL,
+    dayEntered INT NOT NULL,
+    CONSTRAINT housemates_PK PRIMARY KEY (id)
+);
+
+CREATE TABLE agency(
+	id INT NOT NULL,
+    agencyName VARCHAR(50),
+    CONSTRAINT agency_PK PRIMARY KEY (id)
+    -- CONSTRAINT agency_FK FOREIGN KEY (id) REFERENCES housemates(id)
+)
+
+-- (2)  Delete the first two housemates evicted. (AC and Ashley)
+
+-- (3)  Show the agency of the housemate (Star Magic or Sparkle)
+
+-- (4) Add a new housemate (Bianca)
